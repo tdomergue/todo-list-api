e2cfd1b13ff9529e0287ab4834f98a6a11966028
Thibaud Domergue <thibaud.domergue@hei.fr>
2021-04-14 16:39:10 +0200

    API create endpoint

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -2,4 +2,24 @@ class Api::V1::TasksController < Api::V1::BaseController
   def index
     @tasks = Task.all
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      :index
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :deadline, :done)
+  end
+
+  def render_error
+    render json: { errors: @task.errors.full_messages },
+      status: :unprocessable_entity
+  end
 end
